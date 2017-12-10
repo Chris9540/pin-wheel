fd7097a1dcdaf5b4ea7aeb18c41508611a88660a
Chris9540 <chris.9540@hotmail.co.uk>
2017-12-10 22:58:26 +0000

    csv file parsing added

--- js/page-functions.js
+++ js/page-functions.js
@@ -1,7 +1,34 @@
-let kanvas = null
+
+window.kanvas = null
+
+var mie = (navigator.appName == "Microsoft Internet Explorer") ? true : false;
+if (!mie) {
+     document.captureEvents(Event.MOUSEMOVE);
+}
+document.captureEvents(Event.MOUSEMOVE);
+document.onmousemove = function (e) {
+    if (!mie) {
+        mouse.x = e.pageX;
+        mouse.y = e.pageY;
+    }
+    else {
+        mouse.x = event.clientX + document.body.scrollLeft;
+        mouse.y = event.clientY + document.body.scrollTop;
+    }
+    return true;
+};
+window.mouse = {
+    x : 0,
+    y : 0
+}
+window.addEventListener("click", function(){
+    if (mouse.x > 300 && sidebar.style.width == "300px") {
+        sidebar.style.width = "0px";
+    }
+});
+
 
 $(document).ready(function() {
-    console.log("doc ready")
     document.getElementById('btn-menu').addEventListener("click", function(){
         if (document.getElementById('sidebar').style.width == "0px") {
             document.getElementById('sidebar').style.width = "300px";
@@ -52,7 +79,7 @@ $(function() {
 });
 
 $(function() {
-    $( "#slider-vertical-height" ).slider({
+    $( "#slider-vertical-height, #slider-vertical-pin" ).slider({
         orientation: "vertical",
         range: "min",
         min: 0,
@@ -65,3 +92,44 @@ $(function() {
     });
 });
 
+window.csvData = null;
+
+
+function parseCSV() {
+    document.getElementById('load').style.display = "inline-block";
+    setTimeout(function(){ 
+        $('#csv').parse({
+            config: {
+                delimiter: "",  // auto-detect
+                newline: "",    // auto-detect
+                quoteChar: '"',
+                header: false,
+                dynamicTyping: false,
+                preview: 0,
+                encoding: "",
+                worker: false,
+                comments: false,
+                step: undefined,
+                complete:   function(results, file) {
+                                console.log("Parsing complete:", results.data);
+                                csvData = results.data;
+                                document.getElementById('load').style.display = "none"
+                                document.getElementById('pass').style.display = "inline-block"
+                            },
+                error: undefined,
+                download: false,
+                skipEmptyLines: false,
+                chunk: undefined,
+                fastMode: undefined,
+                beforeFirstChunk: undefined,
+                withCredentials: undefined
+            },
+            error: function(err, file, inputElem, reason) {
+                console.log("The Error: " + err)
+                console.log("The Reason: " + reason)
+                document.getElementById('load').style.display = "none"
+                document.getElementById('fail').style.display = "inline-block"
+            },
+        });
+    },50);
+}
